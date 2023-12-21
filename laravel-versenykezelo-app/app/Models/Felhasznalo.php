a963f16c54da7405291cd22792d823763f067f9d
Zsofenszki Kristóf <zsofenszki.kristof@gmail.com>
2023-12-21 11:42:18 +0100

    User átnevezése Felhasznalora

--- laravel-versenykezelo-app/app/Models/Felhasznalo.php
+++ laravel-versenykezelo-app/app/Models/Felhasznalo.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class Felhasznalo extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
