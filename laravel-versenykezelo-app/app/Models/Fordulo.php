bfbaac4487748152572192d720ea751daa22cae7
Zsofenszki Kristóf <zsofenszki.kristof@gmail.com>
2023-12-21 11:39:48 +0100

    Modellek hozzáadása

---
+++ laravel-versenykezelo-app/app/Models/Fordulo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Fordulo extends Model
+{
+    use HasFactory;
+}
